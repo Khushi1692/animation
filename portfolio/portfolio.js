e7569fd584b898a0506dce786b40ade3a5183654
Khushi Kathrotiya <kathrotiyakhushi@gmail.com>
2025-05-08 16:58:06 +0530

    complete portfolio page

---
+++ portfolio/portfolio.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    document.querySelector(".navbar").classList.add("active");
+    document.querySelector(".slide-from-left").classList.add("active");
+    document.querySelector(".slide-from-right").classList.add("active");
+    document.querySelector(".anim-from-left").classList.add("active");
+    document.querySelector(".anim-from-right").classList.add("active");
+    document.querySelector(".anim-from-bottom").classList.add("active");
+  });
+  
