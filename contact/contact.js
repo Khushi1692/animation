8f724f0aee715860a10362f4ffafd66b1fcfd8f3
Khushi1692 <kathrotiyakhushi@gmail.com>
2025-05-10 10:51:15 +0530

    completed all the task from my side

--- contact/contact.js
+++ contact/contact.js
@@ -13,3 +13,11 @@ function sendMail(event){
           alert("Failed to send email: " + JSON.stringify(error));
       });
 }
+
+
+window.addEventListener("load", () => {
+  document.querySelector(".navbar").classList.add("active");
+  document.querySelector(".slide-from-left").classList.add("active");
+  document.querySelector(".slide-from-right").classList.add("active");
+ 
+});
