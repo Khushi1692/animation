7e7bbf8c6267a527c23e6c266f419bde66e74069
Khushi1692 <kathrotiyakhushi@gmail.com>
2025-05-19 23:24:32 +0530

    just add mega menu remaning styling

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const designer = document.getElementById("designer");
 const faceImg = document.getElementById("face-img");
 
 const defaultImg = "assests/demo1.png";
-const designerImg = "assests/coloredface.png";
+const designerImg = "assests/colored_face.png";
 const coderImg = "assests/simple_face1.png";
 
 face.addEventListener("mousemove", (e) => {
@@ -55,34 +55,4 @@ closeMenu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
   mobileMenu.classList.remove('active');
   overlay.classList.remove('active');
-});
-
-// Dropdown toggle logic
-document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
-  toggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    const dropdownMenu = this.nextElementSibling;
-
-    // Close others
-    document.querySelectorAll('.dropdown-menu').forEach(menu => {
-      if (menu !== dropdownMenu) menu.classList.remove('active');
-    });
-
-    dropdownMenu.classList.toggle('active');
-  });
-});
-
-document.querySelectorAll('.dropdown-sub > a').forEach(subToggle => {
-  subToggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    const subMenu = this.nextElementSibling;
-
-    // Close others at the same level
-    const siblingSubmenus = this.parentElement.parentElement.querySelectorAll('.dropdown-submenu');
-    siblingSubmenus.forEach(menu => {
-      if (menu !== subMenu) menu.classList.remove('active');
-    });
-
-    subMenu.classList.toggle('active');
-  });
 });
